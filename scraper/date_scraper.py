0565ea175837451d1b062f8d16e42255a50c1781
Sicelumusa <musaqwabe@gmail.com>
2026-01-02 11:05:38 +0200

    Implemented date scraper function

--- scraper/date_scraper.py
+++ scraper/date_scraper.py
@@ -2,4 +2,11 @@ from bs4 import BeautifulSoup
 
 def scrape_date(driver):
     """Scrapes the date from the iframe."""
-    pass
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    right_div = soup.find("div", id="right2")
+
+    if not right_div:
+        return None
+
+    date_element = right_div.find("b")
+    return date_element.get_text(strip=True) if date_element else None
